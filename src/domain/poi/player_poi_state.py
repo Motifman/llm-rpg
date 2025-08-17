65b3157cc479ec299a85d7141deba1ee62f60fc3
Kei Minagawa <minagawa@fun.bio.keio.ac.jp>
2025-08-17 20:35:00 +0900

    feat: poiシステムを実装 - PlayerPOIStateは各プレイヤーの探索済みPOIを管理 - POIの探索を行うドメインサービスを追加 - POI関連レポジトリのインターフェイスを追加

---
+++ src/domain/poi/player_poi_state.py
@@ -0,0 +1,19 @@
+from typing import Dict, Set, List
+from src.domain.poi.poi_exploration import POIExplorationResult
+
+
+class PlayerPOIState:
+    """プレイヤーPOI状態集約ルート"""
+    def __init__(self, player_id: str):
+        self._player_id = player_id
+        self._discovered_pois: Dict[int, Set[int]] = {}  # spot_id -> poi_ids
+        self._exploration_history: List[POIExplorationResult] = []
+    
+    def record_exploration(self, spot_id: int, result: POIExplorationResult):
+        """探索結果記録（ドメインロジック）"""
+        if result.success:
+            if spot_id not in self._discovered_pois:
+                self._discovered_pois[spot_id] = set()
+            self._discovered_pois[spot_id].add(result.poi_id)
+        
+        self._exploration_history.append(result)
---
+++ src/domain/poi/poi_enum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class POIType(Enum):
+    TREASURE = "treasure"
+    MONSTER_LAIR = "monster_lair"
+    SECRET_PASSAGE = "secret_passage"
+    INFORMATION = "information"
