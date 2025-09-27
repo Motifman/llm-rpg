ab73168ec82c73c265495053ac5b3b331cfae96b
Kei Minagawa <minagawa@fun.bio.keio.ac.jp>
2025-09-28 00:19:49 +0900

    refactor: 古いSNSドメイン層ファイルを削除
    - src/domain/sns/以下の古いドメイン層ファイルを削除
    - base_sns_aggregate.py, base_sns_event.pyを削除
    - block.py, follow.py, like.py, mention.py, subscribe.pyを削除
    - post_aggregate.py, post_content.py, post_event.pyを削除
    - reply.py, reply_aggregate.py, reply_event.pyを削除
    - sns_enum.py, sns_user.py, sns_user_event.pyを削除
    - user_aggregate.py, user_profile.pyを削除

--- src/domain/sns/sns_enum.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class PostVisibility(Enum):
-    PUBLIC = "public"
-    FOLLOWERS_ONLY = "followers_only"
-    SPECIFIED_USERS = "specified_users"
-    PRIVATE = "private"
