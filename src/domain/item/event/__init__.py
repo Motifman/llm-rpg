a8a50c7b43a47f42fc6361f73711888a995d0282
Motifman <150673522+Motifman@users.noreply.github.com>
2025-10-06 02:14:35 +0900

    refactor: src/domain/itemのインポート構造を更新
    - 各サブパッケージの__init__.pyを更新
    - ItemExceptionを追加
    - ItemRepositoryインターフェースを更新
    - DurabilityとItemEffectの既存実装を維持しつつ統合

---
+++ src/domain/item/event/__init__.py
@@ -0,0 +1,7 @@
+from .item_event import ItemUsedEvent, ItemBrokenEvent, ItemRepairedEvent
+
+__all__ = [
+    "ItemUsedEvent",
+    "ItemBrokenEvent",
+    "ItemRepairedEvent",
+]
--- src/domain/item/repository/item_repository.py
+++ src/domain/item/repository/item_repository.py
@@ -1,30 +1,36 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import List, Optional
 
 from src.domain.common.repository import Repository
-from src.domain.item.entity.item import Item
+from src.domain.item.value_object.item_spec import ItemSpec
+from src.domain.item.value_object.item_spec_id import ItemSpecId
 from src.domain.item.enum.item_enum import ItemType, Rarity
 
 
-class ItemRepository(Repository[Item], ABC):
-    """アイテム基本情報のリポジトリインターフェース"""
+class ItemSpecRepository(Repository[ItemSpec], ABC):
+    """アイテム仕様のリポジトリインターフェース"""
 
     @abstractmethod
-    def find_by_type(self, item_type: ItemType) -> List[Item]:
+    def find_by_id(self, item_spec_id: ItemSpecId) -> Optional[ItemSpec]:
+        """アイテム仕様IDで検索"""
+        pass
+
+    @abstractmethod
+    def find_by_type(self, item_type: ItemType) -> List[ItemSpec]:
         """アイテムタイプで検索"""
         pass
 
     @abstractmethod
-    def find_by_rarity(self, rarity: Rarity) -> List[Item]:
+    def find_by_rarity(self, rarity: Rarity) -> List[ItemSpec]:
         """レアリティで検索"""
         pass
 
     @abstractmethod
-    def find_tradeable_items(self) -> List[Item]:
+    def find_tradeable_items(self) -> List[ItemSpec]:
         """取引可能なアイテムを検索"""
         pass
 
     @abstractmethod
-    def find_by_name(self, name: str) -> Optional[Item]:
+    def find_by_name(self, name: str) -> Optional[ItemSpec]:
         """名前で検索"""
         pass
